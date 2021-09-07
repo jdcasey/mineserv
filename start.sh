f7a8d2df1aa5f52555622814480db0b8da2b7125
John Casey <jdcasey@commonjava.org>
2021-09-07 17:20:00 -0500

    add handling for plugins from /usr/local/mineserv/plugins

--- start.sh
+++ start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-MS=/usr/local/mineserv/etc
+MS=/usr/local/mineserv
 
 cd /world || exit -1
 
-if [ -d /plugins ]; then
+if [ -d $MS/plugins ]; then
 	cp -rf /plugins /world
 fi
 
-cp -rf $MS/* /world
+if [ -d $MS/etc ]; then
+	cp -rf $MS/etc/* /world
+fi
 
 exec java $JVM_OPTS -jar /usr/local/lib/mineserv/paper.jar "$PAPER_ARGS"
