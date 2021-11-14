afa2ce5835b81567653241af905db705cde259d1
John Casey <jdcasey@commonjava.org>
2021-11-14 16:07:26 -0600

    move the world storage into /usr/local/mineserv
    Hosting on google (and probably others) with the current setup requires paying for additional storage (with certain minimum sizes that we don't really use). This change consolidates it to one storage system, where we can make more efficient use of the storage minimums.

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 MS=/usr/local/mineserv
+WORLD=$MS/world
 
-cd /world || exit -1
+cd $WORLD || exit -1
 
 if [ -d $MS/plugins ]; then
-	mkdir /world/plugins 2>&1 > /dev/null
-	cp -rf /plugins/* /world/plugins
+	mkdir $WORLD/plugins 2>&1 > /dev/null
+	cp -rf $MS/plugins/* $WORLD/plugins
 fi
 
 if [ -d $MS/etc ]; then
-	cp -rf $MS/etc/* /world
+	cp -rf $MS/etc/* $WORLD
 fi
 
 exec java $JVM_OPTS -jar /usr/local/lib/mineserv/paper.jar "$PAPER_ARGS"
