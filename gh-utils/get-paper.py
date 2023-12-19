5d6c86d69cfac1e160c3580f5dc64793f80045c9
John Casey <jdcasey@commonjava.org>
2023-12-18 20:03:09 -0600

    use python scripts to pull latest releases of paper and most plugins, instead of downloads + static URL

---
+++ gh-utils/get-paper.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import requests
+
+PAPER_VERSIONS = "https://api.papermc.io/v2/projects/paper/"
+PAPER_BUILDS_BASE = "https://api.papermc.io/v2/projects/paper/versions/"
+PAPER_OUTFILE = "paper.jar"
+
+TIMEOUT = 60
+
+resp = requests.get(PAPER_VERSIONS, timeout=TIMEOUT)
+resp.raise_for_status()
+version_data = resp.json()
+
+ver_tuples = []
+longest = 1
+for ver_str in version_data["versions"]:
+    # print(ver_str)
+    try:
+        tup = [int(part) for part in ver_str.split(".")]
+        # print(tup)
+        ver_tuples.append(tup)
+        longest = max(longest, len(tup))
+    except ValueError as e:
+        print(f"Skipping version: {ver_str}")
+
+
+selected = []
+for idx in range(longest):
+    highest = 0
+    for tup in ver_tuples:
+        matches = True
+        for i in range(idx):
+            if tup[i] != selected[i]:
+                matches = False
+
+        if matches:
+            val = tup[idx] if len(tup) > idx else 0
+            highest = max(highest, val)
+
+    selected.append(highest)
+    highest = 0
+
+selected_version = ".".join([str(v) for v in selected])
+print(f"Version: {selected_version}")
+
+resp = requests.get(PAPER_BUILDS_BASE + selected_version)
+resp.raise_for_status()
+version_data = resp.json()
+
+build = str(sorted(version_data["builds"])[-1])
+
+url = f"https://api.papermc.io/v2/projects/paper/versions/{selected_version}/builds/{build}/downloads/paper-{selected_version}-{build}.jar"
+resp = requests.get(url, timeout=5*TIMEOUT)
+resp.raise_for_status()
+with open(PAPER_OUTFILE, 'wb') as fhandle:
+    fhandle.write(resp.content)
+
---
+++ gh-utils/get-plugins.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import os
+from typing import Any, Optional
+
+import requests
+
+OUTDIR = "plugins"
+
+DIRECT_DOWNLOAD = {
+    "Geyser-Spigot.jar": "https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot"
+}
+
+PLUGINS = {
+    "Multiverse-Core": "Multiverse/Multiverse-Core",
+    "Multiverse-Portals": "Multiverse/Multiverse-Portals",
+    "Multiverse-SignPortals": "Multiverse/Multiverse-SignPortals"
+}
+
+HEADERS = {
+    "Accept": "application/vnd.github+json",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+TIMEOUT = 60
+
+
+def get_release(repo_key: str) -> dict[str, Any]:
+    """ Find the latest release for a given repo and return its id and name"""
+
+    resp = requests.get(f"https://api.github.com/repos/{repo_key}/releases/latest", headers=HEADERS, timeout=TIMEOUT)
+    resp.raise_for_status()
+    data = resp.json()
+
+    return {"id": data["id"], "name": data["name"]}
+
+
+def get_jar_url(repo_key: str, release_id: int) -> Optional[dict[str, str]]:
+    """ Find a JAR asset for the given repo and release ID, and return its URL"""
+
+    resp = requests.get(f"https://api.github.com/repos/{repo_key}/releases/{release_id}/assets", headers=HEADERS, timeout=TIMEOUT)
+    resp.raise_for_status()
+    data = resp.json()
+
+    for asset in data:
+        name = asset["name"]
+        if name.endswith(".jar"):
+            # print(f"{asset['name']} with type: {asset['content_type']}")
+            return {"name": name, "url": asset["browser_download_url"]}
+
+    return None
+
+
+def download(name: str, url: str) -> None:
+    plugin_file = os.path.join(OUTDIR, name)
+
+    resp = requests.get(url, timeout=5 * TIMEOUT)
+    resp.raise_for_status()
+    with open(plugin_file, 'wb') as fhandle:
+        fhandle.write(resp.content)
+
+    print(plugin_file)
+
+
+for k, v in PLUGINS.items():
+    rel_data = get_release(v)
+    plugin_download = get_jar_url(v, rel_data["id"])
+    download(plugin_download["name"], plugin_download["url"])
+
+for pname, purl in DIRECT_DOWNLOAD.items():
+    download(pname, purl)
+
